b51495d0717b5ddb783527f01148567e9dbf2c72
Luzaks <kanemekanik@gmail.com>
2019-12-18 12:11:04 -0600

    Set up the first GET action with it's controller and it's view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def index
+        @name = "I am the Index action!"
+    end
+    def list
+        @list_url = "I am the Users action!"
+    end
+    def help
+        @help_url = "I am the Help action!"
+    end
+    def about
+        @about_url = "I am the About action!"
+    end
 end
